5fd1000b7ead62999a4756e47892e42afb613b27
Fayyazalam439 <160386224+Fayyazalam439@users.noreply.github.com>
2024-06-03 23:22:15 +0500

    Created project..currency-converter.

---
+++ main.ts
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+const curr: any = {
+    USD: 1,      
+    EUR: 0.92,
+    GBP: 0.79,
+    INR: 83.46,
+    PKR: 278.10,    //Base currency
+    CAD: 0.95,
+    SGD: 1,
+    BHD: 0.65,
+};
+
+let userAnswer = await inquirer.prompt(
+    [
+        {
+            name: 'from',
+            message: 'Enter from currency',
+            type: 'list',
+            choices:['USD', 'EUR', 'GBP', 'INR', 'PKR', 'CAD', 'SGD', 'BHD']
+
+        },
+        {
+            name: 'to',
+            message: 'Enter from currency',
+            type: 'list',
+            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'CAD', 'SGD', 'BHD']
+        },
+        {
+            name: 'amount',
+            message: 'Enter your amount',
+            type: 'number'
+        }
+    ]
+);
+
+let userFromCurrency = userAnswer.from;
+let userToCurrency = userAnswer.to;
+let fromAmount = curr[userAnswer.from];
+let toAmount = curr[userAnswer.to];
+let amount = userAnswer.amount;
+
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(`Converted Currency Amount: ${convertedAmount}`);
+
+
+
+
